4ae98bd71d2013323e22e0f0d26f862314265648
Dylan Harries <Dylan.Harries@csiro.au>
2019-05-15 10:40:40 +1000

    Remove old functions

--- climate_indices/__init__.py
+++
@@ -1,6 +0,0 @@
-from .aao import calc_aao, calc_aao_from_anomalies, calc_aao_index
-from .ao import calc_ao, calc_ao_from_anomalies, calc_ao_index
-
-
-__all__ = ['calc_aao', 'calc_aao_from_anomalies', 'calc_aao_index',
-           'calc_ao', 'calc_ao_from_anomalies', 'calc_ao_index']
