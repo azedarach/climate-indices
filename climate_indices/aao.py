ed35ed7d9e68693c583d10822063f42a8d506719
Dylan Harries <Dylan.Harries@csiro.au>
2019-08-08 14:56:35 +1000

    Use where rather than sel

--- climate_indices/aao.py
+++ climate_indices/aao.py
@@ -166,7 +166,8 @@ def calculate_aao_pc_index(anom_data, eofs_data,
                            normalization=1):
     n_eofs = 1
 
-    eof_data = eofs_data.sel({EOF_DIM_NAME: aao_mode})
+    eof_data = eofs_data.where(eofs_data[EOF_DIM_NAME] == aao_mode,
+                               drop=True)
 
     pos_phase_pattern = _fix_phase(eof_data, time_field=time_field,
                                    lat_field=lat_field)
